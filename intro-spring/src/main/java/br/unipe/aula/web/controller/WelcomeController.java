54f2a5172af80790aa5dcfa9f7a148bdd4520760
Tatiane <thathy23@gmail.com>
2024-03-22 09:27:03 -0300

    Primeiro commit

---
+++ intro-spring/src/main/java/br/unipe/aula/web/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package br.unipe.aula.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WelcomeController {
+	
+	@RequestMapping(value = "/teste", method = RequestMethod.GET)
+	public String welcome() {
+		return "welcome";
+	}
+
+}
---
+++ intro-spring/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Bem vindo</title>
+</head>
+<body>
+	<h1>Bem vindo, projeto em construção...</h1>
+	<p>Programação Avançada, Spring MVC</p>
+</body>
+</html>
